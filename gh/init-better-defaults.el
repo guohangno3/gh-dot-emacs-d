aa8667c6ba4dcc54864c6513e04c0ed99048608e
guohangno3 <guohangno3@163.com>
2016-11-07 23:25:27 +0800

    add eol config

--- gh/init-better-defaults.el
+++ gh/init-better-defaults.el
@@ -107,7 +107,23 @@
 (require 'dired-x)
 (setq dired-dwim-target t)
 
+;;;;; C-q C-m to generate (^M)
+(defun hidden-dos-eol ()
+  "Do not show (^M) in files containing mixed UNIX and DOS line ending."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\ [])
+  )
 
+(defun remove-dos-eol()
+  "Replace DOS eolns CR LF with Unix eolns CR"
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (while (search-forward "\r" nil t)
+      (replace-match ""))
+    )
+)
 
 
 
