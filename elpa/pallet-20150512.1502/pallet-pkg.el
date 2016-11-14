71ffb45a16db0734cd44c53c4a2941da7017a4e2
guohangno3 <guohangno3@163.com>
2016-11-14 09:25:08 +0800

    delete elpa/pallet

--- elpa/pallet-20150512.1502/pallet-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pallet" "20150512.1502" "Manage your packages with Cask." 'nil 'url: "https://github.com/rdallasgray/pallet" :keywords '(" elpa" " package"))
