ce2af5699a318d664e701b9db62a881458556ad5
guohangno3 <guohangno3@163.com>
2016-11-14 18:56:53 +0800

    nothing

--- my/init-packages.el
+++ my/init-packages.el
@@ -97,6 +97,8 @@
     mwe-log-commands
 
     ;; pallet
+
+    iimage
     
     )
   "Default packages"
@@ -251,6 +253,10 @@
 
 
 ;; (mwe:open-command-log-buffer t)
+
+;; iimage mode
+(autoload 'iimage-mode "iimage" "Support Inline image minor mode." t)
+(autoload 'turn-on-iimage-mode "iimage" "Turn on Inline image minor mode." t)
 
 
 
