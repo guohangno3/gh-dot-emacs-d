526c4b276d7b158ce0ef3c0de4e9a5e1fa75c508
guohangno3 <guohangno3@163.com>
2016-11-16 18:58:23 +0800

    org add task track and state

--- my/init-org.el
+++ my/init-org.el
@@ -63,6 +63,22 @@
 
 (require 'org-pomodoro)
 (setq org-pomodoro-long-break-length 40)
+;;; 跟踪子任务完成情况
+
+(defun org-summary-todo (n-done n-not-done)
+      "Switch entry to DONE when all subentries are done, to TODO otherwise."
+      (let (org-log-done org-log-states)   ; turn off logging
+        (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+(add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
+
+;;; 任务状态
+(setq org-todo-keywords
+    '((sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")
+      (sequence "TODO(t!)" "|" "DONE(d@)" "CANCELED(c@/!)")
+     ))  
+
+
+
 
 (provide 'init-org)
 
